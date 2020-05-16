848ebde891a4b221fc06440469dfeb3a48043b67
Gabriel Neri <neri.gabriel78@gmail.com>
2020-05-15 21:10:34 -0300

    Finalizacao dos esquemas da recyclerview

--- app/src/main/java/com/example/recyclerviewlab/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewlab/MainActivity.java
@@ -1,32 +1,47 @@
 package com.example.recyclerviewlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    // UTILIZADO PARA LOG DE ERROS
+    // public static final String TAG = MainActivity.class.getSimpleName();
+
     private List<String> stringList = new ArrayList<String>();
+    private RecyclerAdapter myAdapter;
+    private Random random           = new Random();
 
     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.recyclerView = findViewById(R.id.recyclerView);
 
+
+        //SETO O LAYOUTMANAGER DA RECYCLERVIEW
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //STARTO MEU ARRAY DE ITENS
         this.populateList();
-
-        recyclerView = findViewById(R.id.recyclerView);
-        //recyclerView.setAdapter();
+        //STARTO MEU ADAPTER INICIANDO COM VALORES
+        this.myAdapter = new RecyclerAdapter(this.stringList);
+        //SETO MEU ADAPTER PARA O RECYCLER VIEW
+        this.recyclerView.setAdapter(myAdapter);
     }
 
     private void populateList() {
+
+        int numero = this.random.nextInt(100);
         for(int i = 0; i <= 10; i++) {
-            stringList.add("Gabriel "+i);
+            stringList.add("Gabriel "+ (numero+i));
         }
     }
 }
--- app/src/main/java/com/example/recyclerviewlab/RecyclerAdapter.java
+++ app/src/main/java/com/example/recyclerviewlab/RecyclerAdapter.java
@@ -1,39 +1,62 @@
 package com.example.recyclerviewlab;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-                                                          // EXTENDE DA MINHA CLASSE INTERNA
+import java.util.List;
+
+// EXTENDE DA MINHA CLASSE INTERNA
+/*   ADAPTER   */
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
 
-    public RecyclerAdapter() {
+    private List<String> stringList;
 
+    /*   VIEWHOLDER - INNER CLASS    */
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        //AQUI INSTANCIO O ELEMENTO DO LAYOUT
+        TextView textViewItem;
+
+        //AQUI INICIO O ELEMENTO DO LAYOUT
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textViewItem = itemView.findViewById(android.R.id.text1);
+        }
     }
+    /* **************************** */
+
+
+    //STARTO O ADAPTER COM A INFORMAÇÃO
+    public RecyclerAdapter(List<String> stringList) {
+        this.stringList = stringList;
+    }
+
+    //STARTO A VIEW DO VIEWHOLDER COM UM LAYOUT
+    //AQUI SERIA ONDE SETO A VIEW QUE QUERO USAR PARA ESSE ADATPER
+    //E NO ONBINDVIEWHOLDER TEREI ACESSO A TODOS OS ELEMENTOS DESSA VIEW
+    //LEMBRANDO QUE ESTES ELEMENTOS DEVEM SER INICIADOS NA CLASSE INNER OU HOLDER
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                                      .inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new RecyclerViewHolder(itemView);
     }
 
+    //AQUI SETO OS VALORES DOS COMPONENTES STARTADAS COM O ONCREATEVIEWHOLDER
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
-
+        holder.textViewItem.setText(this.stringList.get(position));
     }
 
+    //PEGO A QUANTIDADE DE ITENS
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-
-
-    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
-
-        public RecyclerViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
+        return this.stringList.size();
     }
 }
